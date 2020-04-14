6d9ff54b399639c384ce0b035b89e6ed0f6f9b2f
Benedictusags <benedictusadean@gmail.com>
2020-04-14 13:01:43 +0700

    update cors

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express = require('express'),
     port = process.env.PORT || 3000,
     bodyParser = require('body-parser'),
     controller = require('./controller');
+	
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
